66e0450610b9a323903112ea6549393ca6ac0617
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-04-16 20:32:40 +0700

    Refactor code structure for improved readability and maintainability

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman About</title>
-</head>
-
-<body>
+<div class="container">
     <h1>About Me</h1>
+    <img src="<?= BASEURL ?>/img/image.png" alt="Bahtiar" width="200" class="rounded-cicrcle shadow">
     <p>Halo, nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya adalah seorang
         <?= $data['pekerjaan'] ?>
     </p>
-</body>
-
-</html>
+</div>
