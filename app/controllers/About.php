5351dbf5b1fe2b8da28cbbc0a38422c88c894230
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-04-16 19:56:45 +0700

    Refactor About and Home controllers to extend base Controller class and implement view rendering; add header, footer, and about page views.

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,24 @@
 <?php
 
-class About
+class About extends Controller
 {
 
-    public function index($nama = "bahtiar", $pekerjaan = "programmer")
+    public function index($nama = "bahtiar", $pekerjaan = "programmer", $umur = 21)
     {
-        echo "Halo nama saya $nama, saya seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+
     }
 }
