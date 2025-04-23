46f678395354ff7a743429e87b067fcaff433ad1
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2025-04-23 18:36:41 +0700

    Add configuration, database handling, and Mahasiswa detail functionality; refactor views for improved data presentation

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -23,29 +23,25 @@ class Mahasiswa_model
     //     ]
     // ];
 
-    // menyambungkan ke database dengan PDO(PHP data object(lebih fleksibel daripada menggunakan mysqli))
-    private $dbh; // database handler(koneksi ke database)
-    private $stmt; // statement(nyimpen query)
-
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        // data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database();
     }
 
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
 
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
